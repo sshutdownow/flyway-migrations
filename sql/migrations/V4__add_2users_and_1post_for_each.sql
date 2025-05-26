b816f43028ebf7a33896df5409f836258e4b1343
Igor Popov <sshutdownow@users.noreply.github.com>
2025-05-26 12:27:11 +0200

    Update V4__add_2users_and_1post_for_each.sql

--- sql/migrations/V4__add_2users_and_1post_for_each.sql
+++ sql/migrations/V4__add_2users_and_1post_for_each.sql
@@ -1,6 +1,6 @@
 
 with userids as (
-  INSERT INTO users (name) VALUES ('user1', 'user2') RETURNING id
+  INSERT INTO users (name) VALUES ('user1'), ('user2') RETURNING id
 )
 INSERT INTO posts (owner_id, text)
 SELECT id, id || ' post text'
